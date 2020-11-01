094e38d912159a6ad058dad2b1945903192a4c39
madams <mtadams42@gmail.com>
2020-11-01 11:21:41 -0700

    Fixed webpack config to prevent bad caching.

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,15 @@ module.exports = {
 
 	output: {
 		path: path.resolve(__dirname, "docs"),
-		filename: "bundle.js",
-		publicPath: "/"
+		filename: 'bundle.js',
+
+		// it's weird why this is necessary, but w/e
+		publicPath: "./"
 	},
 
 	devServer: {
 		compress: true,
+		publicPath: "/",
 		contentBase: path.join(__dirname, 'src', 'public'),
 
 		stats: {
@@ -24,6 +27,14 @@ module.exports = {
 		},
 		port: 9000
 	},
+
+	plugins: [new HtmlWebpackPlugin({
+		template: "src/public/index.html",
+
+		// use hash here instead of in the actual generated filename
+		// for version control sanity, mostly
+		hash: true
+	})],
 
 	resolve: {
 		extensions: [".json", ".ts", ".js", ".css", ".scss"],
